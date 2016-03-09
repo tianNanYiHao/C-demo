278c4585256f80b6f5386c797e6a0c62462a8eae
851085835@qq.com <851085835@qq.com>
2016-03-09 14:22:25 +0800

    sfs

---
+++ textField输入4数字自动空格/textField输入4数字自动空格/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  textField输入4数字自动空格
+//
+//  Created by Aotu on 16/1/19.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
