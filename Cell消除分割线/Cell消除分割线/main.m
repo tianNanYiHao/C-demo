b0b76ada58114cbb0f26a6a8582a7e9416033fe9
851085835@qq.com <851085835@qq.com>
2016-03-09 14:19:58 +0800

    123

---
+++ Cell消除分割线/Cell消除分割线/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Cell消除分割线
+//
+//  Created by Aotu on 16/1/18.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
