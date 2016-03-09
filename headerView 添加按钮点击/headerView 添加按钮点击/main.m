a05c56b64b3d23aef2f5aac467bebd7b0e2c769e
851085835@qq.com <851085835@qq.com>
2016-03-09 14:19:08 +0800

    32

---
+++ headerView 添加按钮点击/headerView 添加按钮点击/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  headerView 添加按钮点击
+//
+//  Created by Aotu on 15/12/23.
+//  Copyright © 2015年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
