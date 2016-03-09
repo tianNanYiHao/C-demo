2ad1492e33090feafc3ff5b09ad6b305cfbfd40d
851085835@qq.com <851085835@qq.com>
2016-03-09 14:18:37 +0800

    12

---
+++ 输入密码 确认支付/1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by Aotu on 15/12/23.
+//  Copyright © 2015年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
