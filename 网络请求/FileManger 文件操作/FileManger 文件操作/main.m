a4f4facfc3fb7a5865266f192dc02393ae50a0ff
851085835@qq.com <851085835@qq.com>
2016-03-11 20:08:41 +0800

    222

---
+++ 网络请求/FileManger 文件操作/FileManger 文件操作/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FileManger 文件操作
+//
+//  Created by Aotu on 16/3/11.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
