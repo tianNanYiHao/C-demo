0b19ab4fb98ed758e52b27587e1f68af31ae3446
851085835@qq.com <851085835@qq.com>
2016-05-25 10:24:12 +0800

    lff

---
+++ 项目传奇/QuickPos_iOS_NewMall/QuickPos/QuickPos/ViewControllers/QucikPay/BankCardBindViewController.h
@@ -0,0 +1,16 @@
+//
+//  BankCardBindViewController.h
+//  QuickPos
+//
+//  Created by 胡丹 on 15/4/8.
+//  Copyright (c) 2015年 张倡榕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class OrderData;
+
+@interface BankCardBindViewController : UIViewController
+
+@property (nonatomic,strong)OrderData *orderData;
+
+@end
---
+++ 项目传奇/QuickPos_iOS_NewMall/QuickPos/QuickPos/model/data/OrderData.h
@@ -0,0 +1,25 @@
+//
+//  OrderData.h
+//  QuickPos
+//
+//  Created by 胡丹 on 15/3/20.
+//  Copyright (c) 2015年 张倡榕. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BaseData.h"
+
+@interface OrderData : BaseData
+
+@property(nonatomic,strong)NSString *orderId;//订单编号
+@property(nonatomic,strong)NSString *orderAmt;//订单金额
+@property(nonatomic,strong)NSString *orderDesc;//订单详情
+@property(nonatomic,strong)NSString *realAmt;//实际交易金额
+@property(nonatomic,strong)NSString *orderAccount;//交易账号
+@property(nonatomic,assign)NSUInteger orderPayType;//账户支付 刷卡支付
+@property(nonatomic,strong)NSString *merchantId;
+@property(nonatomic,strong)NSString *productId;
+
+@property(nonatomic,assign)BOOL mallOrder;
+
+@end
