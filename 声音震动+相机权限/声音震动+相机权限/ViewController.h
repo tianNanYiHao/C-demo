5e24e054aee335a9b2d5b15553af974ff091b837
851085835@qq.com <851085835@qq.com>
2016-06-30 10:57:59 +0800

    11

---
+++ 声音震动+相机权限/声音震动+相机权限/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  声音震动+相机权限
+//
+//  Created by Aotu on 16/6/30.
+//  Copyright © 2016年 Aotu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
