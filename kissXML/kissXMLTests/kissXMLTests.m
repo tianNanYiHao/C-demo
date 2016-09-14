3bf214f85429945c29a3e22fe44523ff1aea5cf3
tianNanYiHao <851085835@qq.com>
2016-09-14 11:19:30 +0800

    123

---
+++ kissXML/kissXMLTests/kissXMLTests.m
@@ -0,0 +1,39 @@
+//
+//  kissXMLTests.m
+//  kissXMLTests
+//
+//  Created by Lff on 16/9/13.
+//  Copyright © 2016年 Lff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface kissXMLTests : XCTestCase
+
+@end
+
+@implementation kissXMLTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
